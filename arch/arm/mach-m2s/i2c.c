ca7a3162d523cba654e392f42fde37aea53a8d06
Lars Bratrud <uberfryx@gmail.com>
2014-07-13 17:07:41 +0200

    commit

--- arch/arm/mach-m2s/i2c.c
+++ arch/arm/mach-m2s/i2c.c
@@ -155,7 +155,7 @@ void __init m2s_i2c_init(void)
 	/*
  	 * Perform board-specific I2C device registration
  	 */
-	if (p == PLATFORM_M2S_SOM || p == PLATFORM_M2S_FG484_SOM) {
+	if (p == PLATFORM_M2S_SOM || p == PLATFORM_M2S_FG484_SOM || p == PLATFORM_SF2_TPC_RCU) {
 #if defined(CONFIG_M2S_MSS_I2C1)
 
 		/*
